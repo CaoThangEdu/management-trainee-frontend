004ba6fe01c33e8ec034ac2e3c80e399f845d3e0
Huynh <72245217+nmhyh@users.noreply.github.com>
2021-04-20 23:24:42 +0700

    layout_company_management

---
+++ src/router/views/CompanyManagement/CompanyManagement.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ListCompanyManagementComponent />
+  </div>
+</template>
+
+<script>
+import ListCompanyManagementComponent from '../../../components/CompanyManagementComponent/ListCompanyManagementComponent/ListCompanyManagementComponent'
+export default {
+  components:{ ListCompanyManagementComponent }
+}
+</script>
+
+<style>
+</style>
