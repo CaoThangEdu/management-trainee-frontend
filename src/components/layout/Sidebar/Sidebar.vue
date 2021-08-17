730a237fdf956b3f633c1e67099af65a8e341ce9
huynhnm@ebk.vn <huynhnm@ebk.vn>
2021-08-17 22:38:52 +0700

    upadte_menu_while_have_token_login

--- src/components/layout/Sidebar/Sidebar.vue
+++ src/components/layout/Sidebar/Sidebar.vue
@@ -31,10 +31,19 @@
 import nav from "././_nav";
 
 import { mapGetters, mapActions } from "vuex";
+import localStorageMixin from "../../../helpers/mixins/localStorageMixin";
 
 export default {
   name: "Sidebar",
   nav,
+  mixins: [localStorageMixin],
+  data() {
+    return {
+      roles: '',
+      navFilter: [],
+      navOrigins: [],
+    }
+  },
   methods: {
     ...mapGetters("layout", ["getSidebarShow", "getSidebarMinimize"]),
     ...mapActions("layout", ["setSidebarShowData", "setSidebarMinimizeData"]),
@@ -45,6 +54,55 @@ export default {
     minimize() {
       return this.getSidebarMinimize();
     },
+    filterNavByRoles(navsFilter, roles) {
+      return navsFilter.filter(itemNavLink => {
+        if(roles.includes(itemNavLink.role)) {
+          return itemNavLink;
+        }
+      });
+    },
+    parseJwt (token) {
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+
+      return JSON.parse(jsonPayload);
+    },
   },
+  created() {
+    if (!localStorageMixin.methods.getLocalStorage('nav-link')){
+      localStorageMixin.methods.setLocalStorage('nav-link', nav);
+    }
+    this.navOrigins = JSON.parse(JSON.stringify(localStorageMixin.methods.getLocalStorage('nav-link')));
+    this.navFilter = JSON.parse(JSON.stringify(this.navOrigins[0]._children));
+    let tokenInfo = localStorageMixin.methods.getLocalStorage('AUTH_TOKEN');
+    if(!tokenInfo) {
+      nav[0]._children = JSON.parse(JSON.stringify(this.navFilter));
+      return;
+    }
+    let accessToken = tokenInfo.accessToken;
+    this.roles = this.parseJwt(accessToken)['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+    for (let index = 0; index < this.navFilter.length; index++) {
+      if (this.navFilter[index].role) {
+        if(!this.roles.includes(this.navFilter[index].role)) {
+          this.navFilter.splice(index, 1);
+          index = index - 1;
+        }
+        continue;
+      }
+      if (this.navFilter[index].items) {
+        // filter các phần tử khớp với role của user đang đăng nhập
+        this.navFilter[index].items = this.filterNavByRoles(this.navFilter[index].items, this.roles);
+      }
+      // kiểm tra lại sau khi filter nếu là empty thì remove
+      if (this.navFilter[index].items && this.navFilter[index].items.length == 0) {
+        this.navFilter.splice(index, 1);
+        index = index - 1;
+      }
+    }
+    nav[0]._children = JSON.parse(JSON.stringify(this.navFilter));
+  }
 };
 </script>
--- src/components/layout/Sidebar/_nav.js
+++ src/components/layout/Sidebar/_nav.js
@@ -1,90 +1,107 @@
 export default [{
-  _name: 'CSidebarNav',
-  _children: [{
-    _name: 'CSidebarNavDropdown',
-    name: 'Lên kế hoạch',
-    router: '/plan',
-    icon: 'cil-speedometer',
-    items: [{
-        name: 'Danh sách kế hoạch',
-        to: '/plan/list'
-      }, {
-        name: "Thêm mới kế hoạch",
-        to: '/plan/create'
+  _name: "CSidebarNav",
+  _children: [
+      {
+          _name: "CSidebarNavDropdown",
+          name: "Lên kế hoạch",
+          router: "/plan",
+          icon: "cil-speedometer",
+          items: [
+              {
+                  name: "Danh sách kế hoạch",
+                  to: "/plan/list",
+                  role: "Admin",
+              },
+              {
+                  name: "Thêm mới kế hoạch",
+                  to: "/plan/create",
+                  role: "Admin",
+              },
+          ],
       },
-    ]
-  },
-  {
-    _name: 'CSidebarNavItem',
-    name: 'Phân công giáo viên',
-    to: '/teacher-assignment',
-    icon: 'cil-drop',
-  },
-  {
-    _name: 'CSidebarNavItem',
-    name: 'Duyệt phiếu giới thiệu',
-    to: '/confirm-referra-file',
-    icon: 'cil-pencil',
-  },
-  {
-    _name: 'CSidebarNavItem',
-    name: 'Quản lý thực tập sinh viên',
-    to: '/student-management-internships',
-    icon: 'cil-cursor',
-  },
-  {
-    _name: 'CSidebarNavItem',
-    name: 'Quản lý công ty',
-    to: '/company-management',
-    icon: 'cil-puzzle',
-  },
-  {
-    _name: 'CSidebarNavItem',
-    name: 'Quản lý sinh viên',
-    to: '/student-management/list',
-    icon: 'cil-user',
-  },
-  {
-    _name: 'CSidebarNavItem',
-    name: 'Quản lý giáo viên',
-    to: '/teacher-management',
-    icon: 'cil-chart-pie',
-  },
-  {
-    _name: 'CSidebarNavItem',
-    name: 'Quản lý hệ - ngành',
-    to: '/trainingsystem-management',
-    icon: 'cil-calculator',
-  },
-  {
-    _name: 'CSidebarNavItem',
-    name: 'Quản lý lớp',
-    to: '/class-management',
-    icon: 'cil-bell',
-  },
-  {
-    _name: 'CSidebarNavItem',
-    name: 'Quản lý thông báo',
-    to: '/notification-management',
-    icon: 'cil-bell',
-  },
-  {
-    _name: 'CSidebarNavItem',
-    name: 'Đăng ký giấy giới thiệu thực tập',
-    to: '/certificate-management',
-    icon: 'cil-pencil',
-},
-{
-  _name: 'CSidebarNavItem',
-  name: 'Xác nhận thực tập',
-  to: '/internship-confirmation',
-  icon: 'cil-pencil',
-},
-{
-  _name: 'CSidebarNavItem',
-  name: 'Upload file',
-  to: '/upload-file',
-  icon: 'cil-pencil',
-},
+      {
+          _name: "CSidebarNavItem",
+          name: "Phân công giáo viên",
+          to: "/teacher-assignment",
+          icon: "cil-drop",
+          role: "Admin",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Duyệt phiếu giới thiệu",
+          to: "/confirm-referra-file",
+          icon: "cil-pencil",
+          role: "Admin",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Quản lý thực tập sinh viên",
+          to: "/student-management-internships",
+          icon: "cil-cursor",
+          role: "Admin",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Quản lý công ty",
+          to: "/company-management",
+          icon: "cil-puzzle",
+          role: "Admin",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Quản lý sinh viên",
+          to: "/student-management/list",
+          icon: "cil-user",
+          role: "Admin",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Quản lý giáo viên",
+          to: "/teacher-management",
+          icon: "cil-chart-pie",
+          role: "Admin",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Quản lý hệ - ngành",
+          to: "/trainingsystem-management",
+          icon: "cil-calculator",
+          role: "Admin",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Quản lý lớp",
+          to: "/class-management",
+          icon: "cil-bell",
+          role: "Admin",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Quản lý thông báo",
+          to: "/notification-management",
+          icon: "cil-bell",
+          role: "Admin",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Đăng ký giấy giới thiệu thực tập",
+          to: "/certificate-management",
+          icon: "cil-pencil",
+          role: "STUDENT",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Xác nhận thực tập",
+          to: "/internship-confirmation",
+          icon: "cil-pencil",
+          role: "STUDENT",
+      },
+      {
+          _name: "CSidebarNavItem",
+          name: "Upload file",
+          to: "/upload-file",
+          icon: "cil-pencil",
+          role: "STUDENT",
+      },
   ],
 }, ];
