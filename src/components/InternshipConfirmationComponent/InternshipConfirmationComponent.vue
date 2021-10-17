9e7badddafdb5e8be581c0a8990099df465b0282
DESKTOP-843VIUE\TRANTAM <tamtv@ebk.vn>
2021-10-17 07:49:26 +0700

    update

--- src/components/InternshipConfirmationComponent/InternshipConfirmationComponent.vue
+++ src/components/InternshipConfirmationComponent/InternshipConfirmationComponent.vue
@@ -3,52 +3,55 @@
 <script>
 import InternshipConfirmationServices from "../../services/internshipconfirmation/InternshipConfirmationServices";
 import ComponentBase from "../common/component-base/ComponentBase";
-import IntershipConfirmationViewModel from "../../view-model/intershipconfirmation/IntershipConfirmationViewModel"
+import IntershipConfirmationViewModel from "../../view-model/intershipconfirmation/IntershipConfirmationViewModel";
 import AlertMessages from "../common/alert/alert-messages/AlertMessages";
 import { mapGetters } from "vuex";
-import AppConfig from '../../../src/app.config.json'
+import AppConfig from "../../../src/app.config.json";
 export default {
   name: "InternshipConfirmationComponent",
   extends: ComponentBase,
-  components: {AlertMessages},
+  components: { AlertMessages },
   data() {
     return {
       keyInternshipConfirmation: {
-      studentId: "",
-      taxCode: "",
-      title: "",
-      companyAddress: "",
-      website: "",
-      owner: "",
-      manager: "",
-      companiesInterviewed: "",
-      status: "notPracticed", //notPracticed, practiced
-      phoneNumber: "",
+        studentId: "",
+        taxCode: "",
+        title: "",
+        companyAddress: "",
+        website: "",
+        owner: "",
+        manager: "",
+        companiesInterviewed: "",
+        status: "notPracticed", //notPracticed, practiced
+        phoneNumber: "",
       },
       intership: "false",
-      confirmed:false,
+      confirmed: false,
       errorMessages: [],
     };
   },
-  async mounted(){
+  async mounted() {
     let keyConfirmation = await this.getInternshipConfirmationAsync();
-    if(keyConfirmation!== undefined){
+    if (keyConfirmation !== undefined) {
       this.confirmed = true;
       this.keyInternshipConfirmation = keyConfirmation;
     }
-    if(this.keyInternshipConfirmation.status === "practiced"){
+    if (this.keyInternshipConfirmation.status === "practiced") {
       this.intership = "true";
     }
   },
   computed: {
     //gọi phương thức từ getter trên store (tên module, tên phương thức) để xử lý dữ liệu
-    ...mapGetters("user", { userProfile: "getUserInfo", tokenKey: "getTokenKey" }),
+    ...mapGetters("user", {
+      userProfile: "getUserInfo",
+      tokenKey: "getTokenKey",
+    }),
   },
   methods: {
-    changeStatusInternship(){
-      if(this.intership === "false"){
+    changeStatusInternship() {
+      if (this.intership === "false") {
         this.keyInternshipConfirmation.status = "notPracticed";
-        return ;
+        return;
       }
       this.keyInternshipConfirmation.status = "practiced";
     },
@@ -57,19 +60,24 @@ export default {
       await this.internshipConfirmationAsync();
     },
 
-    async internshipConfirmation(){
-      if(this.confirmed === true){
+    async internshipConfirmation() {
+      if (this.confirmed === true) {
         await this.updateInternshipConfirmationAsync();
         return;
       }
       await this.internshipConfirmationAsync();
     },
 
-    validInternshipConfirmation(){
-      if(this.intership === "false" && this.keyInternshipConfirmation.companiesInterviewed === ""){
-        this.errorMessages = ["Vui lòng nhập <span>Những công ty đã phỏng vấn</span>."];
+    validInternshipConfirmation() {
+      if (
+        this.intership === "false" &&
+        this.keyInternshipConfirmation.companiesInterviewed === ""
+      ) {
+        this.errorMessages = [
+          "Vui lòng nhập <span>Những công ty đã phỏng vấn</span>.",
+        ];
       }
-      if(this.intership === "true"){
+      if (this.intership === "true") {
         let viewModel = new IntershipConfirmationViewModel();
         viewModel.setFields(this.keyInternshipConfirmation);
         this.errorMessages = viewModel.isValid();
@@ -77,7 +85,6 @@ export default {
     },
 
     async internshipConfirmationAsync() {
-
       this.validInternshipConfirmation();
       if (this.errorMessages.length > 0) {
         return;
@@ -85,7 +92,9 @@ export default {
       this.keyInternshipConfirmation.studentId = this.userProfile.mssv;
       this.showLoading();
       let api = new InternshipConfirmationServices();
-      let response = await api.internshipConfirmationAsync(this.keyInternshipConfirmation);
+      let response = await api.internshipConfirmationAsync(
+        this.keyInternshipConfirmation
+      );
       this.showLoading(false);
       if (!response.isOK) {
         this.showNotifications(
@@ -95,6 +104,7 @@ export default {
         );
         return;
       }
+      this.confirmed = true;
       this.showNotifications(
         "success",
         `${AppConfig.notification.title_default}`,
@@ -111,13 +121,13 @@ export default {
       this.showLoading(false);
 
       if (!response.isOK) {
-        this.showNotifications(
+        return this.showNotifications(
           "error",
           `${AppConfig.notification.title_default}`,
           response.errorMessages
         );
-        return;
       }
+      this.confirmed = true;
       this.keyInternshipConfirmation = response.data;
       this.showNotifications(
         "success",
@@ -126,12 +136,12 @@ export default {
       );
     },
 
-    async getInternshipConfirmationAsync(){
-      let filter ={
+    async getInternshipConfirmationAsync() {
+      let filter = {
         studentId: this.userProfile.mssv,
-        status: ""
-      }
-      console.log(filter)
+        status: "",
+      };
+      console.log(filter);
       this.showLoading();
       let api = new InternshipConfirmationServices();
       let response = await api.fliterInternshipConfirmationAsync(filter);
@@ -145,7 +155,7 @@ export default {
         return;
       }
       return response.data[0];
-    }
+    },
   },
 };
 </script>
