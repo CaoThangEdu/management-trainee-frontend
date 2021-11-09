4f291869582c202d7939cf5d0644d293a9ada974
DESKTOP-843VIUE\TRANTAM <tamtv@ebk.vn>
2021-11-09 21:07:12 +0700

    reset password

--- src/view-model/user/changePasswordViewModel.js
+++ src/view-model/user/changePasswordViewModel.js
@@ -3,23 +3,41 @@ export default class ChangePasswordViewModel extends ViewModel {
     constructor() {
         super()
         this.fields = {
-            adminPassword: null,
+            oldPassword: null,
             newPassword: null,
+            confirmNewPassword: null,
         }
     }
 
     getValidations() {
         return {
-            adminPassword: {
-                label: 'Mật khẩu admin',
+            oldPassword: {
+                label: 'Mật khẩu cũ',
                 rules: {
                     require: '',
                 }
             },
             newPassword: {
                 label: 'Mật khẩu mới',
+                rules: {                    
+                    require: '',
+                    minLength: {
+                        min_value: 6,
+                    },
+                    password:{
+                        equal:false,
+                        equal_data:this.fields.oldPassword,
+                    }
+                }
+            },
+            confirmNewPassword: {
+                label: 'Xác nhận mật khẩu mới',
                 rules: {
                     require: '',
+                    password:{
+                        equal:true,
+                        equal_data:this.fields.newPassword,
+                    }
                 }
             },
         };
---
+++ src/view-model/user/resetPasswordViewModel.js
@@ -0,0 +1,30 @@
+import ViewModel from '../viewModel'
+export default class ResetPasswordViewModel extends ViewModel {
+    constructor() {
+        super()
+        this.fields = {
+            adminPassword: null,
+            newPassword: null,
+        }
+    }
+
+    getValidations() {
+        return {
+            adminPassword: {
+                label: 'Mật khẩu admin',
+                rules: {
+                    require: '',
+                }
+            },
+            newPassword: {
+                label: 'Mật khẩu mới',
+                rules: {
+                    require: '',
+                }
+            },
+        };
+    }
+    isValid() {
+        return super.isValid(this.getValidations())
+    }
+}
